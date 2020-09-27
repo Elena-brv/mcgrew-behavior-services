69252cb6cdc036606ac56faf2ad6471a70b51eee
Elena-brv <leena.brv@gmail.com>
2020-09-27 15:57:21 +0300

    Landing page

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
-# Base layout template with Gulp, SCSS and Stylelint
-1. Create a repo using this template
-1. Replace `<your_account>` and `<repo_name>` with your Github username and the new repo name
-    - [DEMO LINK](https://<your_account>.github.io/<repo_name>/)
+[DEMO LINK](https://Elena-brv.github.io/McGrew_behavior_services/)
+
+PSD to HTML Responsive + Retina
+
+Source files: https://www.dropbox.com/sh/6ud20zt79ci330x/AAADGp6X2UPHXmNQJaC1DkLra?dl=0
+
+Fonts: Google Web Font
+
+## Requirements:
+1. You should code one page
+2. Responsive - there is no design so that you can use your solution for mobile and tablets
+3. Use SASS or SCSS
+4. No Twitter Bootstrap
+5. Retina
+6. Use at least jQuery version 2 or 3
+7. JS Slider 001.png - use FadeIn/Out effect without pagination auto slide. You should use Slick Slider plugin.
+8. JS Mobile default menu. Menu and dropdowns should look correct on all resolutions, tablet, and mobile.
+9. JS Multi-level dropdown menu - there is no design so that you can use your solution
+10. All links should go to https://www.google.com
+11. A phone link should be linked to a phone number
+12. An email link should be linked to an email
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,70 @@
 'use strict';
+
+const mediumDesktopBreakPoint = 1200;
+const mobileBreakPoint = 768;
+const smallMobileBreakPoint = 480;
+
+$(function() {
+  $('.slider').slick({
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 3,
+    variableWidth: true,
+
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          variableWidth: false,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          variableWidth: false,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          variableWidth: false,
+        },
+      },
+    ],
+  });
+
+  $('.slider-game').slick({
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    fade: true,
+  });
+
+  $('.nav__item').on('mouseover', function() {
+    $('.submenu').hide();
+    $(this).children().show();
+  });
+
+  $('.nav__item').on('mouseout', function() {
+    $('.submenu').hide();
+  });
+
+  $('.submenu__item').on('mouseover', function() {
+    $('.submenu__list').hide();
+    $(this).children().show();
+  });
+
+  $('.submenu__item').on('mouseout', function() {
+    $('.submenu__list').hide();
+  });
+
+  $('.header__mobile-menu').on('click', function() {
+    $('.nav').toggleClass('nav-mobile');
+    $('.header__toggler').toggleClass('header__toggler--active');
+  });
+});
